776ccc59330af59fc94eb973482ff13e3a2dacc8
amiletti <a.miletti@enegan.it>
2022-08-03 11:50:28 +0200

    rimesso namespace2

--- pigeon-master/libraries/pigeon.php
+++ pigeon-master/libraries/pigeon.php
@@ -17,6 +17,7 @@ class Pigeon
 
 	public $temporary_routes = array();
 	public $namespace = '';
+	public $namespace2 = '';
 
 	/* --------------------------------------------------------------
      * GENERIC METHODS
